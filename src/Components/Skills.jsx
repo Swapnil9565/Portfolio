798d12615ab698f83cef0949b1e7e0ef7cc308f6
Swapnil Mohite <swapnilmohite9565@gmail.com>
2024-11-25 10:10:53 +0530

    Feat:Skills Page

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,43 +1,46 @@
 import React from "react";
 
 const Skills = ({ mode }) => {
+  const mySkills = [
+    { id: 1, skill: "HTML5", percentage: 95 },
+    { id: 2, skill: "CSS3", percentage: 90 },
+    { id: 3, skill: "JavaScript", percentage: 70 },
+    { id: 4, skill: "MongoDB", percentage: 65 },
+    { id: 5, skill: "React", percentage: 70 },
+    { id: 6, skill: "Tailwind", percentage: 95 },
+  ];
   return (
     <div
       className={`${
         mode === "dark" ? "text-white" : "text-black"
       } pt-5 md:pt-10`}>
       <h1 className='text-center font-bold text-[#d53ff9] text-4xl uppercase underline'>
-        My Skills
+        <span
+          className={`${
+            mode == "dark" ? "text-white" : "text-black"
+          } underline`}>
+          My{" "}
+        </span>
+        Skills
       </h1>
-      <div className='flex justify-center gap-10'>
-        <div className='w-1/4'>
-          <h1 className="my-3">HTML5</h1>
-          <div  className="w-full border-2 border-orange-500 rounded-md">
-            <div className='w-[90%] border-4 border-orange-500'></div>
-          </div>
-          <h1 className="my-3">CSS3</h1>
-          <div className="w-full border-2 border-orange-500 rounded-md">
-            <div className='w-[85%] border-4  border-orange-500'></div>
-          </div>
-          <h1 className="my-3">JavaScript</h1>
-          <div className="w-full border-2 border-orange-500 rounded-md">
-            <div className='w-[70%] border-4  border-orange-500'></div>
-          </div>
-        </div>
-        <div className='w-1/4'>
-          <h1>React</h1>
-          <div className="w-full border-2 border-orange-500 rounded-md">
-            <div className='w-[65%] border-4 border-orange-500'></div>
-          </div>
-          <h1>MySql</h1>
-          <div className="w-full border-2 border-orange-500 rounded-md">
-            <div className='w-[80%] border-4  border-orange-500 '></div>
-          </div>
-          <h1>Tailwind CSS</h1>
-          <div className="w-full border-2 border-orange-500 rounded-md">
-            <div className='w-[95%] border-4  border-orange-500 '></div>
-          </div>
-        </div>
+      <p className='text-lg text-center my-4 leading-7 md:text-sm '>
+        Proficient in designing and developing full-stack web applications using
+        the MERN stack MongoDB, Express.js, React.js, Node.js{" "}
+      </p>
+      <div className='w-3/4 grid  grid-cols-1 md:w-2/3 md:grid-cols-2 md:gap-x-20 my-20 mx-auto'>
+        {mySkills.map(({ id, skill, percentage }) => {
+          return (
+            <div key={id} className='mb-7 md:mb-5'>
+              <h1 className="font-semibold text-xl md:text-sm">{skill}</h1>
+              <div className='w-full bg-[#959595] rounded-md h-[15px]'>
+                <div
+                  className="bg-red-500 text-center text-white rounded-md h-[15px]" style={{width:`${percentage}%`}}>
+                  <span className="h-[30px] w-[30px] rounded-[50%] bg-[#222] float-right mt-[-15px] md:mt-[-10px] mr-[-20px] text-md text-white flex justify-center items-center p-6 md:p-4 md:text-xs">{percentage}%</span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
